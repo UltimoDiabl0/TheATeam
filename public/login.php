06e7763ec42cb4d4fc0288c5ca9b10bfde6e2cbd
Cody Boyd <jackjacksonrus@gmail.com>
2020-10-23 18:23:48 -0400

    Daniels work

---
+++ public/login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+    try{
+      $config = parse_ini_file("db.ini");
+      $dbh = new PDO($config['dsn'], $config['username'],$config['password']);
+
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if (isset($_POST['username']) and isset($_POST['password']) ){
+        foreach ($dbh->query("loginUser(\"".$_POST['username']."\",\"".$_POST['password']."\")") as $row) {
+            $_SESSION["loggedIn"] = true;
+            $_SESSION["userID"] = $row[0];
+            header("Location: calendar.php");
+            return;
+          }
+        header("Location: login.html");
+      }
+
+
+      } catch (PDOException $e) {
+        print "\nError! " . $e->getMessage()."<br/>";
+        die();
+      }
+
+
+ ?>
---
+++ public/signUp.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+    try{
+      $config = parse_ini_file("db.ini");
+      $dbh = new PDO($config['dsn'], $config['username'],$config['password']);
+
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if (isset($_POST['username']) and isset($_POST['password']) ){
+        $dbh->query("CALL createUser(\"".$_POST['username']."\",\"".$_POST['password']."\",\"".$_POST['timezone']."\")");
+        header("Location: login.html");
+      }
+
+
+      } catch (PDOException $e) {
+        print "\nError! " . $e->getMessage()."<br/>";
+        die();
+      }
+
+
+ ?>
