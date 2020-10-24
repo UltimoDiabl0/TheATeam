405aac8b2b618f6a5ff4811045fd579482b84171
Cody Boyd <jackjacksonrus@gmail.com>
2020-10-24 03:49:07 -0400

    Accounted for SQL injection, created Confirm password logic.

--- public/login.php
+++ public/login.php
@@ -7,7 +7,14 @@ session_start();
 
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       if (isset($_POST['username']) and isset($_POST['password']) ){
-        foreach ($dbh->query("SELECT loginUser(\"".$_POST['username']."\",\"".$_POST['password']."\")") as $row) {
+
+        // Preventing SQL Injection via prepare statement, and escaping variables to use as plain text and not code
+        $userHandler = $dbh->prepare('SELECT loginUser( :username, :password)');
+        $userHandler->bindParam(':username', $_POST['username']);
+        $userHandler->bindParam(':password', $_POST['password']);
+        $userHandler->execute();
+
+        foreach ($userHandler->fetch(PDO::FETCH_ASSOC) as $row) {
             if (!is_null($row[0])) {
               $_SESSION["loggedIn"] = true;
               $_SESSION["userID"] = $row[0];
@@ -15,12 +22,12 @@ session_start();
               return;
             }
           }
-        header("Location: login.html");
+        header("Location: index.html");
       }
 
       } catch (PDOException $e) {
         print "\nError! " . $e->getMessage()."<br/>";
         die();
       }
-      
+
  ?>
--- public/signUp.php
+++ public/signUp.php
@@ -6,9 +6,25 @@ session_start();
       $dbh = new PDO($config['dsn'], $config['username'],$config['password']);
 
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      if (isset($_POST['username']) and isset($_POST['password']) ){
-        $dbh->query("CALL createUser(\"".$_POST['username']."\",\"".$_POST['password']."\",\"".$_POST['timezone']."\")");
-        header("Location: login.html");
+      if (isset($_POST['username']) and isset($_POST['password']) and ($_POST['password'] == $_POST['passConfirm']) and ($_POST['username'] != "") and ($_POST['password'] != "") and ($_POST['passConfirm'] != "")){
+
+        // Preventing SQL Injection via prepare statement, and escaping variables to use as plain text and not code
+        $userHandler = $dbh->prepare('CALL createUser( :username, :password, :timezone)');
+        $userHandler->bindParam(':username', $_POST['username']);
+        $userHandler->bindParam(':password', $_POST['password']);
+        $userHandler->bindParam(':timezone', $_POST['timezone']);
+
+        if($userHandler->execute()){
+            //User created successful account
+            header("Location: index.html");
+        } else {
+            //User failed to create successful account
+            header("Location: signUp.html");
+        }
+
+      } else {
+        //User failed to input correct information
+        header("Location: signUp.html");
       }
 
       } catch (PDOException $e) {
