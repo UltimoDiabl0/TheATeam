b76d3ed338436ed6c68d9c069fdbc7500b107bbd
Eric Belt <ejbelt@mtu.edu>
2020-11-01 18:30:51 -0500

    Some more changes to calendar.js

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -15,8 +15,6 @@ function writeToDay(Date startTime, Date endTime){
 
 }**/
 
-const curDayBlock = createTimeBlock(-1, Date.now(), Date.now(), "NullTimeBlock");
-
 buildCalendar();
 
 function buildCalendar() {
@@ -30,7 +28,7 @@ function buildCalendar() {
   //Made a class called 'timeBlockDummy' which just is there to distinquish the time block paragraphs from the one before
   //saying that this is the user's timeblocks.
   var timeBlockQuery = document.querySelectorAll("p.timeBlockDummy");
-  var timeBlocks = [curDayBlock];
+  var timeBlocks = [];
 
   //Loop through each of the p blocks and get the time necessary for input into the timeBlocks.
   timeBlockQuery.forEach(function(time){
@@ -43,9 +41,32 @@ function buildCalendar() {
 
   console.log(timeBlocks);
 
+
   //Now the juicy stuff, making the calendar appear!
   //The container to hold each day of the current week.
   var dayContainer = document.querySelector('calendar');
+
+  //TODO: Determine the current day's Day of week int
+  //      loop through and find each day before and make sections for those
+  //      make a section for it
+  //      Loop through and find each day in the future and make a section for it.
+  //      Finally, add the necessary information into each section.
+  //      How? For now we should just put the raw info, we can make it prettier later.
+
+  //Start of the week.
+  var sunday = pointToSunday(new Date(curTime));
+  var timeBlockCopy = [...timeBlocks];
+
+  for (let i=0; i<7; i++){
+      //Again each day is worth 86400000ms
+      var day = new Date(sunday.getTime()+(86400000*i));
+
+      
+
+  }
+
+  console.log(timeBlockCopy)
+
 
 }
 
@@ -73,4 +94,12 @@ function convertToTime(time){
   //Now we can make a proper date and return it.
   const date = new Date(realTime);
   return date;
+}
+
+function pointToSunday(date){
+  //One day is 86400000 milliseconds
+  var dayOfWeek = date.getDay();
+  var time = date.getTime();
+  var newDate = new Date(time-(86400000*dayOfWeek));
+  return newDate;
 }
